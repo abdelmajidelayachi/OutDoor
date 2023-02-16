6dd98d327390305e7237b551a89cf1791590170c
Elmehdi <93981356+Raikonerr@users.noreply.github.com>
2023-02-16 10:55:25 +0100

    build :  methods service but needs datasource

--- OrderService/src/main/java/com/outdoor/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/outdoor/orderservice/controller/OrderController.java
@@ -1,14 +1,23 @@
 package com.outdoor.orderservice.controller;
 
 import com.outdoor.orderservice.dto.OrderRequest;
+import com.outdoor.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
 
+
+    private final OrderService orderService;
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest){
-        return "Order Placed";
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+
     }
 }
--- OrderService/src/main/java/com/outdoor/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/outdoor/orderservice/service/OrderService.java
@@ -4,21 +4,30 @@ import com.outdoor.orderservice.dto.OrderItemsDto;
 import com.outdoor.orderservice.dto.OrderRequest;
 import com.outdoor.orderservice.model.Order;
 import com.outdoor.orderservice.model.OrderItems;
+import com.outdoor.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final OrderRepository orderRepository;
+
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
        List<OrderItems> orderItems = orderRequest.getOrderItemsDto().stream()
                 .map(this::mapToDto)
                 .toList();
+
         order.setOrderItems(orderItems);
+
+        orderRepository.save(order);
     }
+
 
     private OrderItems mapToDto(OrderItemsDto orderItemsDto) {
         OrderItems orderItems = new OrderItems();
@@ -27,5 +36,6 @@ public class OrderService {
         orderItems.setSkuCode(orderItemsDto.getSkuCode());
         return orderItems;
     }
+
 
 }
